baa5db7c2fd977a3ff02526e45470d3e34ac5388
Mohamed Hamdy <opgghamdy@gmailcom>
2024-03-18 00:00:06 +0200

    upload files

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'sam-building';
+}
